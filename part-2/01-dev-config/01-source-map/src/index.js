dbbecb407d22f32986375b5b458b9299a5183241
you_5c24 <you_5c24@163.com>
2022-11-10 14:34:52 +0800

    feat: source-map

--- part-2/01-dev-config/01-source-map/src/index.js
+++ part-2/01-dev-config/01-source-map/src/index.js
@@ -1,1 +1,12 @@
-console.log("hello world~");
+class Test {
+  constructor() {
+    this.str = "hello world";
+  }
+
+  say() {
+    console.log(this.str);
+  }
+}
+
+const t1 = new Test();
+t1.say();
--- part-2/01-dev-config/01-source-map/webpack.config.js
+++ part-2/01-dev-config/01-source-map/webpack.config.js
@@ -2,13 +2,29 @@ const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
 module.exports = {
-  entry: "./src/index.js",
+  entry: {
+    index: "./src/index.js",
+  },
   output: {
-    filename: "[name].js",
+    filename: "scripts/[name].js",
     path: path.resolve(__dirname, "./dist"),
     clean: true,
   },
   mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+    ],
+  },
   plugins: [
     new HtmlWebpackPlugin({
       template: "./src/index.html",
@@ -16,6 +32,7 @@ module.exports = {
       inject: "body",
     }),
   ],
+  devtool: "cheap-module-source-map",
   devServer: {
     static: "./dist",
   },
