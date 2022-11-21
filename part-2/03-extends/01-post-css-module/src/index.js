5abaaa47b9b6c9c8feaab67a0e4d0338799f6624
you_5c24 <you_5c24@163.com>
2022-11-21 10:41:14 +0800

    feat: add other file

---
+++ part-2/03-extends/01-post-css-module/src/index.js
@@ -0,0 +1,5 @@
+import style from "./index.css";
+
+const div = document.createElement("div");
+div.classList.add(style.box);
+document.body.appendChild(div);
---
+++ part-2/03-extends/01-post-css-module/webpack.config.js
@@ -0,0 +1,24 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          {
+            loader: "css-loader",
+            options: {
+              modules: true,
+            },
+          },
+          "postcss-loader",
+        ],
+      },
+    ],
+  },
+  plugins: [new HtmlWebpackPlugin()],
+};
