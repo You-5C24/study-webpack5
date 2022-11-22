26d86f4525aa6a1c3a320837efbef0c11256789b
you_5c24 <you_5c24@163.com>
2022-11-22 15:27:29 +0800

    feat: worker-pool

---
+++ part-2/08-build-performance/02-worker-pool/src/index.js
@@ -0,0 +1,12 @@
+class A {
+  constructor(name) {
+    this.name = name;
+  }
+
+  print() {
+    console.log(this.name);
+  }
+}
+
+const a = new A("5C24");
+a.print();
---
+++ part-2/08-build-performance/02-worker-pool/webpack.config.js
@@ -0,0 +1,27 @@
+module.exports = {
+  mode: "development",
+  entry: "./src/index.js",
+  output: {
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+          options: {
+            presets: ["@babel/preset-env"],
+          },
+        },
+      },
+      {
+        loader: "thread-loader",
+        options: {
+          workers: 2,
+        },
+      },
+    ],
+  },
+};
